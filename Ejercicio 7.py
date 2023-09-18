a54e13a8aef78ac3cf328bd91307317450cf9f1b
Sebastian Correa Fuentes <144928705+SebastianXOXY@users.noreply.github.com>
2023-09-18 10:47:37 -0500

    aaa

---
+++ Ejercicio 7.py
@@ -0,0 +1,53 @@
+#Los coclos para la programacion son secuenciales que permiten la realizacion de ejercicios
+#funcionales para poder abreviar o automatizar codigo, esta el ciclo for que significa
+#ciclo while que significa mientras que
+
+print ("Tipo de programa que incluya un ciclo")
+
+print ("----------Inicio del ciclo----------")
+
+for i in range (10):
+    print (i*2)
+
+print ("Finalice el ciclo")
+print("\U0001F600")
+
+#El programa que vamos a realizar tiene como finalidad realizar un ciclo en donde le pida al usuario
+#ingresar hasta 3 un numero, en este caso la edad, y cuando genere la variable correcta 
+#ejecutar en pantalla la siguiente accion, por ejemplo: digita tu numero de cedula, 3. Digite si desea 
+#saber en numero de silla donde fue asignado.
+
+print ("Admision para ingresar")
+  
+
+#
+
+Nombre = str (input("Ingresa tu Nombre"))
+
+
+
+Edad = int (input("Ingresa tu Edad"))
+if Edad < 18:
+    print ("Eres menor de edad, no puedes continuar")
+else:
+    print ("Eres mayor de edad")
+
+
+Identificacion = float (input("Numero de Identificacion"))
+
+
+
+Ubicacion = (str("Su numero de silla es: "))
+#for lista_sillas in range:
+    
+
+
+lista_sillas = ["A1","A2","A3","A4","B1","B2","B3","B4"]
+ 
+from random import randrange, choice
+print (choice(["A1","A2","A3","A4","B1","B2","B3","B4"]))
+
+#for Edad in range 
+
+ 
+
